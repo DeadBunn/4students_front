39118d8fb45d7d24bc9bf4ca09de8b464d2e3ad5
Dmitriy Mikhaylov <greatbox04@mail.ru>
2023-12-10 18:53:59 +0300

    Добавил фильтрацию по названию и типу

--- src/http/OrderApi.js
+++ src/http/OrderApi.js
@@ -10,13 +10,28 @@ export const fetchTypes = async () => {
     const {data} = await $host.get('api/type') //вставить айпишник 
     return data
 }
-// export const createOrder = async () => { //вставить что передаем
-//     const {data} = await $authHost.post('api/device', device) //вставить айпишник
-//     return data
-// }
+export const createOrder = async (orderBody) => {
+    const { data } = await $authHost.post('api/ads', orderBody);
+    return data;
+};
 // Аналогично
-export const fetchOrders = async () => {
-    const {data} = await $host.get('api/ads')
-    return data
-}
+export const fetchOrders = async (filter) => {
+    console.log("Filter Object:", filter);
+    const config = {
+        params:
+            {
+                type: filter.type,
+                title: filter.title
+            }
+    };
 
+    try{
+        const { data } = await $host.get('api/ads', config);
+        return data;
+    }
+    catch (error){
+        console.error("Ошибка ошибка ошибка:", error.response.data);
+        throw error;
+    }
+
+};
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,56 +1,64 @@
-import React , {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import NavigationBar from "../components/NavigationBar";
 import ItemList from "../components/ItemList";
 import Image from "../images/Vector.png"
 import {observer} from "mobx-react-lite";
 import {Context} from "../index";
-import {fetchBrands, fetchOrders, fetchTypes} from "../http/OrderApi";
+import {createOrder, fetchBrands, fetchOrders, fetchTypes} from "../http/OrderApi";
 
-const Main= observer(() =>{
-
+const Main = observer(() => {
     const { order } = useContext(Context);
+    const [searchTitle, setSearchTitle] = useState(``);
+    const [searchType, setSearchType] = useState('SERVICE'); // Default search type
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const orders = await fetchOrders();
-                order.setDevices(orders);
-            } catch (error) {
-                console.error('Ошибка получения заказов:', error);
-            }
+    const handleSearch = () => {
+        const filters = {
+            title: searchTitle,
+            type: searchType,
         };
 
-        fetchData()
-    }, [order]);
+        fetchOrders(filters).then((data) => {
+            order.setDevices(data);
+        });
+    };
 
+    useEffect(() => {
+        // Assuming you want to load orders on the initial render
+        handleSearch();
+    }, []); // Empty dependency array means it runs only once on mount
 
-    return(
+    return (
         <div>
-            <NavigationBar/>
-            <div style={{display:"flex",padding:"0px 50px"}}>
+            <NavigationBar />
+            <div style={{ display: 'flex', padding: '0px 50px' }}>
                 <form className="SearchTitle">
-                <input 
-                className="SearchTitleTxt" 
-                placeholder="Искать..." 
-                type="text"
-                />
-                <button className="SearchTitleBtn"><img alt="img" className="SearchTitleImg" src={Image}/></button>
+                    <input
+                        className="SearchTitleTxt"
+                        placeholder="Искать..."
+                        type="text"
+                        value={searchTitle}
+                        onChange={(e) => setSearchTitle(e.target.value)}
+                    />
+                    <button className="SearchTitleBtn" onClick={handleSearch}>
+                        <img alt="img" className="SearchTitleImg" src={Image} />
+                    </button>
                 </form>
-                <form >
-                    <select className="SelectField"  size="1">
-                        <option selected value="order">Ищу заказ</option>
+                <form>
+                    <select
+                        className="SelectField"
+                        size="1"
+                        value={searchType}
+                        onChange={(e) => setSearchType(e.target.value)}
+                    >
+                        <option value="order">Ищу заказ</option>
                         <option value="Service">Ищу услугу</option>
                     </select>
                 </form>
             </div>
-            <div style={{padding:"0px 50px"}}>
-               <ItemList/>
+            <div style={{ padding: '0px 50px' }}>
+                <ItemList />
             </div>
-            
         </div>
-        
-        
-        
     );
 });
 
